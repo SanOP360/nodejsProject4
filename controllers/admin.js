f08bd02ebfe37149ac818f966d8c45986f953857
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-07 23:05:28 +0530

    used SQL DATABASE

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,9 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,6 +32,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
+  console.log(prodId)
   Product.findById(prodId, product => {
     if (!product) {
       return res.redirect('/');
@@ -55,14 +64,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([products, metadata]) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
+
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
